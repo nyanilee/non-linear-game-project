d67abdfd2a9bbd7b3fc1d4c781dd2bcb1d48baa8
Unknown <drdgvhbh@gmail.com>
2017-11-21 01:20:17 -0500

    update animator

--- non-linear-game/Assets/Scripts/GameObjects/Player/PlayerAnimatorHandler.cs
+++ non-linear-game/Assets/Scripts/GameObjects/Player/PlayerAnimatorHandler.cs
@@ -30,14 +30,20 @@
             var distance = Vector3.Distance(
                 this.lastPosition,
                 this.transform.position);
-            this.animator.SetBool("isMoving", distance > Mathf.Epsilon);
+            this.animator.SetBool("isMoving", distance > 0.01);
 
             // Set direction of sprite
             var euler = this.transform.rotation.eulerAngles;
-            this.transform.rotation =
-                (this.transform.position - this.lastPosition).x >= 0
-                    ? Quaternion.Euler(euler.x, 90, euler.z)
-                    : Quaternion.Euler(euler.x, -90, euler.z);
+            if ((this.transform.position - this.lastPosition).x
+                > Mathf.Epsilon) {
+                this.transform.rotation =
+                    Quaternion.Euler(euler.x, 90, euler.z);
+            }
+            else if ((this.transform.position - this.lastPosition).x
+                     < -Mathf.Epsilon) {
+                this.transform.rotation =
+                    Quaternion.Euler(euler.x, -90, euler.z);
+            }
 
             this.lastPosition = this.transform.position;
         }
